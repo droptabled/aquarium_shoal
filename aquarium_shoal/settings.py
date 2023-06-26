cffcc639f5de9997d0852cfd66d85dc37bbb00bd
Mike <minghan.mike.li@gmail.com>
2023-06-26 19:36:22 -0400

    regenerate secret and move to env var

--- aquarium_shoal/settings.py
+++ aquarium_shoal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mdekzps=g7u932x$p6-0q)%9a($if*rwg-q&nhp___ljqz+44k'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /d/Projects/venv/aquarium-shoal-env/Scripts/activate
+source ./secrets.sh
+python manage.py runserver
