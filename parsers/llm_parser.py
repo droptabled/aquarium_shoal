2eec7511c68e9426ca4b9820204c78c759bcff9d
Mike <minghan.mike.li@gmail.com>
2023-08-01 22:43:23 -0400

    Iterative parsing instead of all in one

--- parsers/llm_parser.py
+++ parsers/llm_parser.py
@@ -3,41 +3,38 @@ import os
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
 class LLMParser:
-    json_schema = {
-        "type": "object",
-        "properties": {
-            # Nomenclature
-            "name": {"type": "string"},
-            "family": {"type": "string"},
-            "origin": {"type": "string"},
-            "size": {"type": "string"},
-            # Tank
-            "tankSize": {"type": "string"},
-            "tankLevel": {"type": "string"},
-            # Care
-            "diet": {"type": "string"},
-            "careDifficulty": {"type": "string"},
-            "breeding": {"type": "string"},
-            "aggressiveness": {"type": "string"},
-            # Water Parameters
-            "pH": {"type": "string"},
-            "gH": {"type": "string"},
-            "kH": {"type": "string"},
-            "temperature": {"type": "string"},
-        }
-    }
+    fields = [
+        # Nomenclature
+        "name",
+        "family",
+        "origin",
+        "fish size",
+        # Tank
+        "tank size",
+        # Care
+        "diet",
+        "care difficulty",
+        "breeding difficulty",
+        "aggressiveness",
+        # Water
+        "pH",
+        "hardness",
+        "carbonate hardness",
+        "temperature",
+    ]
     tokenizer = AutoTokenizer.from_pretrained(os.environ['LLM_SOURCE'])
     model = AutoModelForSeq2SeqLM.from_pretrained(os.environ['LLM_SOURCE'])
 
     @classmethod
     def parse(cls, website_html: str) -> dict:
+        generator = pipeline("text2text-generation", model=cls.model, tokenizer=cls.tokenizer)
+        results = {}
+        for field in cls.fields:
+            prompt = f'''
+            Use this document: {website_html}
+            What is the required {field}?'''
+            generated_data = generator(prompt, max_length=100)
+            results[field] = generated_data[0]["generated_text"]
+
         breakpoint()
-        generator = pipeline("text2text-generation", model=cls.model)
-        prompt = f'''
-        Use this document: {website_html}
-        Extract important fields into a JSON object
-        If a field can't be found, return "Not Found"
-        Use the following schema: {cls.json_schema}'''
-        generated_data = generator(prompt)
-        breakpoint()
-        return generated_data
+        return results
--- parsers/views.py
+++ parsers/views.py
@@ -1,7 +1,9 @@
+from bs4 import BeautifulSoup
 from django.template import loader
 from django.http import HttpResponse
 from .models import Fish
 from .forms import FishParserForm
+from .llm_parser import LLMParser
 import requests
 
 # Create your views here.
@@ -10,7 +12,23 @@ def index(request):
     return HttpResponse(template.render({'form': FishParserForm()}, request))
 
 def create(request):
-    url = request.POST.get("link", "")
-    result = requests.get(url)
+    fish_id = request.POST.get("fish", "")
+    fish = Fish.objects.get(id=fish_id)
+
+    url = request.POST.get("url", "")
+    result = requests.get(url, allow_redirects=True)
+
+    html_content = BeautifulSoup(result.content, features="html.parser")
+    tables = html_content.find_all("table")
+    text = ""
+
+    # all tables
+    #for table in tables:
+    #    text += " " + table.get_text(" ")
+
+    # first table
+    text += tables[0].get_text(" ")
+    parsed_json = LLMParser.parse(text)
     breakpoint()
+
     return HttpResponse("Completed parse")
